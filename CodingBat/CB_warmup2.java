1eec5cf723d383e9e071273cc6e445819f0bd23a
petitemiu <petite.chu@gmail.com>
2017-11-26 23:45:25 +0300

    Add 3rd solution to 2nd CodingBat Warmup

--- CodingBat/CB_warmup2.java
+++ CodingBat/CB_warmup2.java
@@ -27,3 +27,15 @@ public String frontTimes(String str, int n) {
   return res;
   
 }
+
+
+//Solution 3
+//Count the number of "xx" in the given string. We'll say that overlapping is allowed, so "xxx" contains 2 "xx".
+
+int countXX(String str) {
+  int len = str.length(), count = 0;
+  for (int i = 0; i < len - 1; ++i)
+    if (str.charAt(i) == 'x' && str.charAt(i + 1) == 'x')
+      ++count;
+  return count;
+}
