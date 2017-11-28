59ffb6ec6466c3d724d6a104e665bb162e9db195
petitemiu <petite.chu@gmail.com>
2017-11-29 00:09:44 +0300

    Add few more solutions for HackeRank Intro

--- HackerRank/HR_Intro.java
+++ HackerRank/HR_Intro.java
@@ -95,3 +95,70 @@ public class Solution {
         System.out.println("Int: " + i);
     }
 }
+
+
+//Solution 5
+//In each line of output there should be two columns: 
+//The first column contains the String and is left justified using exactly 15 characters. 
+//The second column contains the integer, expressed in exactly 3 digits; if the original input has less than three digits, you must pad your output's leading digits with zeroes.
+
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+            Scanner sc=new Scanner(System.in);
+            System.out.println("================================");
+            for(int i=0;i<3;i++){
+                String s1=sc.next();
+                int x=sc.nextInt();
+                System.out.printf("%-15s%03d\n", s1, x);
+            }
+            System.out.println("================================");
+
+    }
+}
+
+
+//Solution 6
+//Given an integer, N, print its first 10 multiples. Each multiple N * i (where 1<=i<=10 ) should be printed on a new line in the form: N x i = result.
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+        for (int i = 1; i <= 10; ++i){
+            System.out.printf("%d x %d = %d\n", N, i, N*i);
+        }
+    }
+}
+
+
+//Solution 7
+//https://www.hackerrank.com/challenges/java-loops/problem
+
+import java.util.*;
+import java.io.*;
+
+class Solution{
+    public static void main(String []argh){
+        Scanner in = new Scanner(System.in);
+        int t=in.nextInt();
+        for(int i=0;i<t;i++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+            int sum = a;
+            for(int j = 0; j < n; ++j){
+                sum += (int)Math.pow(2, j) * b;
+                System.out.printf("%d ", sum);
+            }
+            System.out.println("");
+        }
+        in.close();
+    }
+}
