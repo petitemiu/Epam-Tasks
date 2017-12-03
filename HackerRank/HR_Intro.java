3735b3505cf9140826b488d98d7f7fb8bfcde718
petitemiu <petite.chu@gmail.com>
2017-12-03 14:54:26 +0300

    Finish HackerRank Intro

--- HackerRank/HR_Intro.java
+++ HackerRank/HR_Intro.java
@@ -277,3 +277,24 @@ public class Solution {
 //Solution 13
 //https://www.hackerrank.com/challenges/java-currency-formatter/problem
 
+import java.util.*;
+import java.text.*;
+
+public class Solution {
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double payment = scanner.nextDouble();
+        scanner.close();
+        
+        Format us = NumberFormat.getCurrencyInstance(Locale.US);
+        Format india = NumberFormat.getCurrencyInstance(new Locale("en", "IN"));
+        Format china = NumberFormat.getCurrencyInstance(Locale.CHINA);
+        Format france = NumberFormat.getCurrencyInstance(Locale.FRANCE);
+        
+        System.out.println("US: " + us.format(payment));
+        System.out.println("India: " + india.format(payment));
+        System.out.println("China: " + china.format(payment));
+        System.out.println("France: " + france.format(payment));
+    }
+}
