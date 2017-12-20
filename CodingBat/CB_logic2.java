443cfab4fea41b1e5878c5a197dc45d0e5a1ecf1
petitemiu <petite.chu@gmail.com>
2017-12-20 21:10:26 +0300

    Finish CodingBat Logic-2

--- CodingBat/CB_logic2.java
+++ CodingBat/CB_logic2.java
@@ -31,16 +31,6 @@ public int luckySum(int a, int b, int c) {
 
 
 //Solution 4
-//Given 3 int values, a b c, return their sum. However, if one of the values is 13 then it does not count towards the sum and values to its right do not count. So for example, if b is 13, then both b and c do not count.
-
-public int luckySum(int a, int b, int c) {
-  return a==13? 0 :
-    b==13 ? a :
-      c==13 ? a+b : a+b+c;
-}
-
-
-//Solution 5
 //Given 3 int values, a b c, return their sum. However, if any of the values is a teen -- in the range 13..19 inclusive -- then that value counts as 0, except 15 and 16 do not count as a teens. Write a separate helper "public int fixTeen(int n) {"that takes in an int value and returns that value fixed for the teen rule. In this way, you avoid repeating the teen code 3 times (i.e. "decomposition"). Define the helper below and at the same indent level as the main noTeenSum().
 
 public int noTeenSum(int a, int b, int c) {
@@ -52,7 +42,7 @@ public int fixTeen(int n){
 }
 
 
-//Solution 6
+//Solution 5
 //For this problem, we'll round an int value up to the next multiple of 10 if its rightmost digit is 5 or more, so 15 rounds up to 20. Alternately, round down to the previous multiple of 10 if its rightmost digit is less than 5, so 12 rounds down to 10. Given 3 ints, a b c, return the sum of their rounded values. To avoid code repetition, write a separate helper "public int round10(int num) {" and call it 3 times. Write the helper entirely below and at the same indent level as roundSum().
 
 public int roundSum(int a, int b, int c) {
@@ -64,7 +54,7 @@ public int round10(int num){
 }
 
 
-//Solution 7
+//Solution 6
 //Given three ints, a b c, return true if one of b or c is "close" (differing from a by at most 1), while the other is "far", differing from both other values by 2 or more. Note: Math.abs(num) computes the absolute value of a number.
 
 public boolean closeFar(int a, int b, int c) {
@@ -74,6 +64,37 @@ public boolean closeFar(int a, int b, int c) {
 }
 
 
-//Solution 8
+//Solution 7
 //Given 2 int values greater than 0, return whichever value is nearest to 21 without going over. Return 0 if they both go over.
 
+public int blackjack(int a, int b) {
+  return a>21 ? (b>21 ? 0 : b) : (b>21 ? a : (a>b ? a : b));    
+}
+
+
+//Solution 8
+//Given three ints, a b c, one of them is small, one is medium and one is large. Return true if the three values are evenly spaced, so the difference between small and medium is the same as the difference between medium and large.
+
+public boolean evenlySpaced(int a, int b, int c) {
+  int max = a>b ? (a>c ? a : c) : b>c ? b : c;
+  int small = a<b ? (a<c ? a : c) : b<c ? b : c;
+  int med = a+b+c-max-small;
+  return (max-med) == (med-small);
+}
+
+
+//Solution 9
+//We want make a package of goal kilos of chocolate. We have small bars (1 kilo each) and big bars (5 kilos each). Return the number of small bars to use, assuming we always use big bars before small bars. Return -1 if it can't be done.
+
+public int makeChocolate(int small, int big, int goal) {
+  if (goal <= big*5+small){
+    int div = goal/5;
+    if(big>=div){
+      return small >= goal-div*5 ? goal-div*5 : -1; 
+    }
+    if(small>=goal-big*5){
+      return goal-big*5;
+    }
+  }
+  return -1;
+}
