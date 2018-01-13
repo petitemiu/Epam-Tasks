473916b28c91c6d55d7ab4fe6264ac6478fd2be1
petitemiu <petite.chu@gmail.com>
2018-01-14 00:39:59 +0300

    Add HackerRank BigNum module

---
+++ HackerRank/HR_BigNum.java
@@ -0,0 +1,21 @@
+//Solution 1
+//https://www.hackerrank.com/challenges/java-biginteger/problem
+
+import java.io.*;
+import java.util.*;
+import java.math.BigInteger;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        BigInteger a = in.nextBigInteger();
+        BigInteger b = in.nextBigInteger();
+        System.out.println( a.add(b));
+        System.out.println( a.multiply(b));
+    }
+}
+
+//Solution 2
+//https://www.hackerrank.com/challenges/java-bigdecimal/problem
+
