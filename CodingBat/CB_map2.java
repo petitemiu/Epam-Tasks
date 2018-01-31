7442c2ff9edfca5ad0a50095a32432192a9928a0
petitemiu <petite.chu@gmail.com>
2018-01-31 10:29:37 +0300

    Finish CodingBat Map-2

--- CodingBat/CB_map2.java
+++ CodingBat/CB_map2.java
@@ -120,3 +120,42 @@ public Map<String, Boolean> wordMultiple(String[] strings) {
 //Solution 8
 //We'll say that 2 strings "match" if they are non-empty and their first chars are the same. Loop over and then return the given array of non-empty strings as follows: if a string matches an earlier string in the array, swap the 2 strings in the array. When a position in the array has been swapped, it no longer matches anything. Using a map, this can be solved making just one pass over the array. More difficult than it looks.
 
+public String[] allSwap(String[] strings) {
+  Map<String, Integer> map = new HashMap();
+  for(int i = 0; i < strings.length; ++i){
+    String firstSym = strings[i].substring(0, 1);
+    Integer index = map.get(firstSym);
+    if(index == null){
+      map.put(firstSym, i);
+    }
+    else{
+      String tmp = strings[i];
+      strings[i] = strings[index];
+      strings[index] = tmp;
+      map.remove(firstSym);
+    }
+  }
+  return strings;
+}
+
+
+//Solution 9
+//We'll say that 2 strings "match" if they are non-empty and their first chars are the same. Loop over and then return the given array of non-empty strings as follows: if a string matches an earlier string in the array, swap the 2 strings in the array. A particular first char can only cause 1 swap, so once a char has caused a swap, its later swaps are disabled. Using a map, this can be solved making just one pass over the array. More difficult than it looks.
+
+public String[] firstSwap(String[] strings) {
+  Map<String, Integer> map = new HashMap();
+  for(int i = 0; i < strings.length; ++i){
+    String firstSym = strings[i].substring(0, 1);
+    Integer index = map.get(firstSym);
+    if(index == null){
+      map.put(firstSym, i);
+    }
+    else if(index != -1){
+      String tmp = strings[i];
+      strings[i] = strings[index];
+      strings[index] = tmp;
+      map.put(firstSym, -1);
+    }
+  }
+  return strings;
+}
