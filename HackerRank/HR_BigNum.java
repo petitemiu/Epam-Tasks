742557675196eab96ed798a4a1c1f7b471d0d76f
petitemiu <petite.chu@gmail.com>
2018-02-13 23:31:03 +0300

    Finish HackerRank BigNumber module

--- HackerRank/HR_BigNum.java
+++ HackerRank/HR_BigNum.java
@@ -16,6 +16,43 @@ public class Solution {
     }
 }
 
+
 //Solution 2
 //https://www.hackerrank.com/challenges/java-bigdecimal/problem
 
+s[n] = "Nope";
+s[n+1] = "Nope";
+Arrays.sort(s,((String number1,String number2) ->  {
+	if (!(number1.equals("Nope")) && !(number2.equals("Nope"))){
+		BigDecimal value1 = new BigDecimal(number1);
+        BigDecimal value2 = new BigDecimal(number2);
+        return value2.compareTo(value1);
+		}
+    return 0;
+}));
+
+
+//Solution 3
+//https://www.hackerrank.com/challenges/java-primality-test/problem
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+   public static void main(String[] args) {
+      Scanner in = new Scanner(System.in);
+      BigInteger n = in.nextBigInteger();
+      in.close();
+      String result = "prime";
+      if (!n.isProbablePrime(1)){
+          result = "not prime";
+      }
+       System.out.println(result);
+      // Write your code here.
+   }
+    
+}
